be70394b612f4c3fad2597c9f94e6dbb84ffa8f2
KANGHO <ariaskkh@gmail.com>
2024-03-30 19:58:13 +0900

    1330 - 두 수 비교하기

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var inputStr = Console.ReadLine();
+        var strList = inputStr.Split(' ');
+        var A = int.Parse(strList[0].ToString());
+        var B = int.Parse(strList[1].ToString());
+
+        if(A > B){
+            Console.WriteLine(">");
+        } else if (A < B) {
+            Console.WriteLine("<");
+        } else {
+            Console.WriteLine("==");
+        }
+    }
+}
