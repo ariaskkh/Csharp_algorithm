b8bf270e2e504e2c79cb20fa8bdfb152e1cdf36d
dean.kim <ariaskkh@gmail.com>
2024-04-28 10:05:00 +0900

    [1343 실] 폴리오미노

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -4,132 +4,76 @@ using System.Text;
 namespace Algorithm;
 public class Program
 {
-	static void Main(string[] args)
-	{
-		var N = int.Parse(Console.ReadLine());
-		Solve(N);
-	}
-
-    //PrintStars(24, 0, 0);
-    //  PrintStars(12, 0, 12);
-    //    PrintStars(6, 0, 18);
-    //      PrintStars(3, 0, 21);
-    //      PrintStars(3, 3, 18);
-    //      PrintStars(3, 3, 24);
-    //    PrintStars(6, 6, 12);
-    //      PrintStars(3, 6, 15);
-    //      PrintStars(3, 9, 12);
-    //      PrintStars(3, 9, 18);
-    //    PrintStars(6, 6, 24);
-    //      PrintStars(3, 6, 27);
-    //      PrintStars(3, 9, 24);
-    //      PrintStars(3, 9, 30);
-    //  PrintStars(12, 12, 0);
-    //    PrintStars(6, 12, 6);
-    //      PrintStars(3, 12, 9);
-    //      PrintStars(3, 15, 6);
-    //      PrintStars(3, 15, 12);
-    //    PrintStars(6, 18, 0);
-    //      PrintStars(3, 18, 3);
-    //      PrintStars(3, 21, 0);
-    //      PrintStars(3, 21, 6);
-    //    PrintStars(6, 18, 12);
-    //      PrintStars(3, 18, 15);
-    //      PrintStars(3, 21, 12);
-    //      PrintStars(3, 21, 18);
-    //  PrintStars(12, 12, 24);
-    //    PrintStars(6, 12, 30);
-    //      PrintStars(3, 12, 33);
-    //      PrintStars(3, 15, 30);
-    //      PrintStars(3, 15, 36);
-    //    PrintStars(6, 18, 24);
-    //      PrintStars(3, 12, 27);
-    //      PrintStars(3, 21, 24);
-    //      PrintStars(3, 21, 30);
-    //    PrintStars(6, 18, 36);
-    //      PrintStars(3, 12, 39);
-    //      PrintStars(3, 21, 36);
-    //      PrintStars(3, 21, 42);
-
-    static void Solve(int N)
-	{
-        var paper = new Paper(N);
-        paper.PrintStar(N, 0, 0);
-        Console.WriteLine(paper.GetString());
-	}
-
-    public class Paper
+    static void Main(string[] args)
     {
-        private bool[][] _isStar;
+        string inputBoard = Console.ReadLine();
+        Solve(inputBoard);
+    }
 
-        public Paper() { }
-        public Paper(int N)
-        {
-            _isStar = Enumerable.Range(0, N)
-                .Select(row => Enumerable.Repeat(false, N * 2 - 1).ToArray()).ToArray();
-        }
+    static void Solve(string inputBoard)
+    {
+        var polyomino = new Polyomino();
+        var coveredBoard = polyomino.SetPolyomino(inputBoard);
+        Console.WriteLine(coveredBoard);
+    }
 
-        public string GetString()
+    class Polyomino
+    {
+        string _board = "";
+        string fail = "-1";
+        public string SetPolyomino(string inputBoard)
         {
-            var sb = new StringBuilder();
-            foreach (var row in _isStar)
+            int count = 0;
+            for (var i = 0; i < inputBoard.Length; i++)
             {
-                sb.Append(new String(row.Select(element => element ? '*' : ' ').ToArray()));
-                sb.Append(Environment.NewLine);
+                if (inputBoard[i] == 'X')
+                {
+                    count++;
+                    if (i != inputBoard.Length - 1)
+                    {
+                        continue;
+                    }
+                }
+
+                if (inputBoard[i] == '.' && count == 0)
+                {
+                    _board += '.';
+                    continue;
+                }
+
+                // (count != 0 && '.' ) or (마지막 X) 인 상태
+                if (count > 0)
+                {
+                    if (count % 2 == 0)
+                    {
+                        int shareDividedByFour = count / 4;
+                        for (int j = 0; j < shareDividedByFour; j++)
+                        {
+                            _board += "AAAA";
+                            count -= 4;
+                        }
+
+                        if (count == 2)
+                        {
+                            _board += "BB";
+                            count -= 2;
+                        }
+                    }
+                    else
+                    {
+                        return fail;
+                    }
+                    if (inputBoard[i] == '.')
+                    {
+                        _board += '.';
+                    }
+                }
             }
-            return sb.ToString();
-        }
-
-        void PrintTinyStar(int row, int column)
-        {
-            _isStar[row][column] = true;
-        }
-
-       
-        public void PrintSmallStar(int row, int column)
-        {
-            var starPositions = new (int Row, int Column)[]
-           {
-               // 3개 행 X 5개 열
-               (0, 2),
-               (1, 1), (1, 3),
-               (2, 0), (2, 1), (2, 2), (2, 3), (2, 4),
-           };
-
-           foreach (var position in starPositions)
-            {
-                var nextRow = row + position.Row;
-                var nextColumn = column + position.Column;
-                PrintTinyStar(nextRow, nextColumn);
-            }
-        }
-
-        public void PrintStar(int N, int row, int column)
-        {
-            if (N == 3)
-            {
-                PrintSmallStar(row, column);
-                return;
-            }
-
-            var starPositions2 = new (int Row, int Column)[]
-            {
-                (0, 0),
-                (1, -1), (1, 1)
-            };
-
-            var nextN = N / 2;
-            foreach (var position in starPositions2)
-            {
-                var nextRow = row + position.Row * nextN;
-                var nextColumn = column + position.Column * nextN + nextN;
-                PrintStar(nextN, nextRow, nextColumn);
-            }
+            return _board;
         }
     }
 
-
-    static IEnumerable<(T1, T2)> IteratonFunction<T1, T2>(IEnumerable<T1> arr1, IEnumerable<T2> arr2)
+static IEnumerable<(T1, T2)> IteratonFunction<T1, T2>(IEnumerable<T1> arr1, IEnumerable<T2> arr2)
     {
         return arr1.Join<T1, T2, bool, (T1, T2)>(arr2, item1 => true, item2 => true, (item1, item2) => (item1, item2));
     }
