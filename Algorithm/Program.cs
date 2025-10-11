3c9f1c9b901d0e417581d63206dd9cb1ba3f1c90
KANGHO <ariaskkh@gmail.com>
2025-10-11 21:13:30 +0900

    18111실 - 마인크래프트 다시

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -4,252 +4,301 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		var gradeInputs = Enumerable.Range(0, 20)
-			.Select(_ => ReadLine().Split(' '))
-			.Select(i => new GradeData(i[0], double.Parse(i[1]), i[2]))
-			.ToList();
-		var result = GradeCalculator.GetAverage(gradeInputs);
-		WriteLine(result);
+		var input = ReadLine().Split(' ').ChangeStrToInt();
+		var sero = input[0];
+		var garo = input[1];
+		var blocksAvailable = input[2];
+		
+		// int[][]
+		var groundTable = Enumerable.Range(0, sero).Select(_ => ReadLine().Split(' ').ChangeStrToInt()).ToArray();
+		var ground = new Ground(groundTable);
+		var user = new User(blocksAvailable);
+		Solve(ground, user);
+	}
+	
+	static void Solve(Ground ground, User user)
+	{
+		var flatteningData = ground.GetFlatteningData(user);
+		if (flatteningData is not null)
+		{
+			WriteLine($"{flatteningData.TimeNeeded} {flatteningData.Height}");
+		}
 	}
 }
 
-public static class GradeCalculator
+public class User
 {
-	public static double GetAverage(List<GradeData> gradeDataList)
+	public int BlocksAvailable { get; init; }
+	
+	public User(int blocksAvailable)
 	{
-		var garaDataListWithoutP = gradeDataList.Where(d => d.SubjectScore != "P").ToList();
-		var totalWeight = garaDataListWithoutP
-				.Sum(d => d.SubjectWeight);
-		var scoreSum = garaDataListWithoutP
-			.Select(d => d.SubjectWeight * d.SubjectScoreNumber)
-				.Sum();
-		return scoreSum / totalWeight;
+		BlocksAvailable = blocksAvailable;
 	}
 }
 
-public class GradeData
+class Ground
 {
-	private readonly static double AP = 4.5;
-	private readonly static double A0 = 4.0;
-	private readonly static double BP = 3.5;
-	private readonly static double B0 = 3.0;
-	private readonly static double CP = 2.5;
-	private readonly static double C0 = 2.0;
-	private readonly static double DP = 1.5;
-	private readonly static double D0 = 1.0;
-	private readonly static double F = 0.0;
-	private readonly static double P = -1.0;
-
-	public GradeData(string subjectName, double subjectWeight, string subjectScore)
+	public int[][] _originalGroundHeight;
+	public Dictionary<int, int> _groundHeightDict;
+	private int _FlattenedGroundheight = 0;
+	private int _minHeight;
+	private int _maxHeight;
+	
+	public Ground(int[][] groundHeight)
 	{
-		SubjectName = subjectName;
-		SubjectWeight = subjectWeight;
-		SubjectScore = subjectScore;
+		_originalGroundHeight = groundHeight;
+		_groundHeightDict = GetGroundHeightDict();
+		_minHeight = _groundHeightDict.Keys.Min();
+		_maxHeight = _groundHeightDict.Keys.Max();
 	}
-
-	public string SubjectName { get; init; }
-	public double SubjectWeight { get; init; }
-	public string SubjectScore { get; init; }
-	public double SubjectScoreNumber => SubjectScore switch
+	
+	private Dictionary<int, int> GetGroundHeightDict()
 	{
-		"A+" => AP,
-		"A0" => A0,
-		"B+" => BP,
-		"B0" => B0,
-		"C+" => CP,
-		"C0" => C0,
-		"D+" => DP,
-		"D0" => D0,
-		"F" => F,
-		"P" => P,
-		_ => 0.0
-	};
+		return _originalGroundHeight
+					.SelectMany(h => h)
+					.GroupBy(h => h)
+					.ToDictionary(g => g.Key, g => g.Count());
+	}
+	
+	public FlatteningResult GetFlatteningData(User user)
+	{
+		var flatteningResult = Enumerable.Range(_minHeight, _maxHeight - _minHeight + 1)
+				.Select(h => CalculateFlatteningData(h, user.BlocksAvailable))
+				.Where(data => data.SufficientBlock)
+				.OrderBy(data => data.TimeNeeded)
+				.ThenByDescending(data => data.Height)
+				.FirstOrDefault();
+		_FlattenedGroundheight = flatteningResult.Height;
+		return flatteningResult;
+	}
+	
+	private FlatteningResult CalculateFlatteningData(int targetHeight, int blocksAvailable)
+	{
+		var digNeeded = GetNumberOfDig(targetHeight);
+		var fillNeeded = GetNumberOfFill(targetHeight);
+		
+		return new FlatteningResult
+		{
+			Dig = digNeeded,
+			Fill = fillNeeded,
+			SufficientBlock = digNeeded + blocksAvailable >= fillNeeded,
+			TimeNeeded = digNeeded * 2 + fillNeeded,
+			Height = targetHeight,
+		};
+	}
+	
+	private int GetNumberOfDig(int targetHeight)
+	{
+		return _groundHeightDict
+					.Select(h => h.Key)
+					.Where(h => h > targetHeight)
+					.Sum(h => (h - targetHeight) * _groundHeightDict[h]);
+	}
+	
+	private int GetNumberOfFill(int targetHeight)
+	{
+		return _groundHeightDict
+					.Select(h => h.Key)
+					.Where(h => h < targetHeight)
+					.Sum(h => (targetHeight - h) * _groundHeightDict[h]);
+	}
+}
+
+public class FlatteningResult
+{
+	public int Dig { get; init; }
+	public int Fill { get; init; }
+	public bool SufficientBlock { get; init; }
+	public int TimeNeeded { get; init; }
+	public int Height { get; init; }
 }
 
 /////////////////////////////  util 함수  ////////////////////////////////
-static T[] CopyArray<T>(T[] array)
-    {
-        T[] newArray = new T[array.Length];
-        for (var i = 0; i < array.Length; i++)
-        {
-            newArray[i] = array[i];
-        }
-        return newArray;
-    }
+public static class Utils
+{
+	static T[] CopyArray<T>(T[] array)
+	{
+		T[] newArray = new T[array.Length];
+		for (var i = 0; i < array.Length; i++)
+	    {
+	        newArray[i] = array[i];
+	    }
+	    return newArray;
+	}
+	
+	static T[][] Copy2DArray<T>(T[][] array)
+	{
+	    T[][] newArray = new T[array.Length][];
+	    for (var i = 0; i < array.Length; i++)
+	    {
+	        newArray[i] = new T[array[i].Length];
+	        for (var j = 0; j < array[i].Length; j++)
+	        {
+	            newArray[i][j] = array[i][j];
+	        }
+	    }
+	    return newArray;
+	}
+	
+	static T[][] Get2DArray<T>(T[][] array, T element)
+	{
+	    T[][] newArray = new T[array.Length][];
+	    for (var i = 0; i < array.Length; i++)
+	        newArray[i] = Enumerable.Repeat(element, array.Length).ToArray();
+	    return newArray;
+	}
+	
+	static Dictionary<char, int> SetDictionary(Dictionary<char, int> charCounts, string inputString)
+	{
+	    foreach (var character in inputString)
+	    {
+	        if (charCounts.ContainsKey(character))
+	        {
+	            charCounts[character]++;
+	        }
+	        else
+	        {
+	            charCounts[character] = 1;
+	        }
+	    }
+	    return charCounts;
+	}
+	
+	
+	static IEnumerable<(T1, T2)> IterationFunction<T1, T2>(IEnumerable<T1> arr1, IEnumerable<T2> arr2)
+	{
+	    return arr1.Join<T1, T2, bool, (T1, T2)>(arr2, item1 => true, item2 => true, (item1, item2) => (item1, item2));
+	}
+	static IEnumerable<(T1, T2, T3)> IterationFunction<T1, T2, T3>(IEnumerable<T1> arr1, IEnumerable<T2> arr2, IEnumerable<T3> arr3)
+	{
+	    return arr1.Join(arr2, item1 => true, item2 => true, (item1, item2) => (item1, item2)).Join(arr3, item => true, item3 => true, (item, item3) => (item.item1, item.item2, item3));
+	}
+	
+	static char[,] Get2dArrayCopy(char[,] square)
+	{
+	    int rows = square.GetLength(0);
+	    int cols = square.GetLength(1);
+	    char[,] copyArr = new char[rows, cols];
+	    for (int i = 0; i < rows; i++)
+	    {
+	        for (int j = 0; j < cols; j++)
+	        {
+	            copyArr[i, j] = square[i, j];
+	        }
+	    }
+	    return copyArr;
+	}
+	
+	static char[,] GetSquareArr(int N)
+	{
+	    char[,] resultArr = new char[N, N];
+	    for (var i = 0; i < N; i++)
+	    {
+	        var tmpRowArr = ReadLine().ToCharArray();
+	        for (var j = 0; j < N; j++)
+	        {
+	            resultArr[i, j] = tmpRowArr[j];
+	        }
+	    }
+	    return resultArr;
+	}
+	
+	static int[,] GetArr(int N, int M)
+	{
+	    int[,] resultArr = new int[N, M];
+	    for (var i = 0; i < N; i++)
+	    {
+	        var tmpRowArr = ReadLine().Split(' ');
+	        for (var j = 0; j < M; j++)
+	        {
+	            resultArr[i, j] = int.Parse(tmpRowArr[j]);
+	        }
+	    }
+	    return resultArr;
+	}
 
-    static T[][] Copy2DArray<T>(T[][] array)
-    {
-        T[][] newArray = new T[array.Length][];
-        for (var i = 0; i < array.Length; i++)
-        {
-            newArray[i] = new T[array[i].Length];
-            for (var j = 0; j < array[i].Length; j++)
-            {
-                newArray[i][j] = array[i][j];
-            }
-        }
-        return newArray;
-    }
-
-    static T[][] Get2DArray<T>(T[][] array, T element)
-    {
-        T[][] newArray = new T[array.Length][];
-        for (var i = 0; i < array.Length; i++)
-            newArray[i] = Enumerable.Repeat(element, array.Length).ToArray();
-        return newArray;
-    }
-
-    static Dictionary<char, int> SetDictionary(Dictionary<char, int> charCounts, string inputString)
-    {
-        foreach (var character in inputString)
-        {
-            if (charCounts.ContainsKey(character))
-            {
-                charCounts[character]++;
-            }
-            else
-            {
-                charCounts[character] = 1;
-            }
-        }
-        return charCounts;
-    }
-
-
-    static IEnumerable<(T1, T2)> IterationFunction<T1, T2>(IEnumerable<T1> arr1, IEnumerable<T2> arr2)
-    {
-        return arr1.Join<T1, T2, bool, (T1, T2)>(arr2, item1 => true, item2 => true, (item1, item2) => (item1, item2));
-    }
-    static IEnumerable<(T1, T2, T3)> IterationFunction<T1, T2, T3>(IEnumerable<T1> arr1, IEnumerable<T2> arr2, IEnumerable<T3> arr3)
-    {
-        return arr1.Join(arr2, item1 => true, item2 => true, (item1, item2) => (item1, item2)).Join(arr3, item => true, item3 => true, (item, item3) => (item.item1, item.item2, item3));
-    }
-
-    static char[,] Get2dArrayCopy(char[,] square)
-    {
-        int rows = square.GetLength(0);
-        int cols = square.GetLength(1);
-        char[,] copyArr = new char[rows, cols];
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                copyArr[i, j] = square[i, j];
-            }
-        }
-        return copyArr;
-    }
-
-    static char[,] GetSquareArr(int N)
-    {
-        char[,] resultArr = new char[N, N];
-        for (var i = 0; i < N; i++)
-        {
-            var tmpRowArr = ReadLine().ToCharArray();
-            for (var j = 0; j < N; j++)
-            {
-                resultArr[i, j] = tmpRowArr[j];
-            }
-        }
-        return resultArr;
-    }
-
-    static int[,] GetArr(int N, int M)
-    {
-        int[,] resultArr = new int[N, M];
-        for (var i = 0; i < N; i++)
-        {
-            var tmpRowArr = ReadLine().Split(' ');
-            for (var j = 0; j < M; j++)
-            {
-                resultArr[i, j] = int.Parse(tmpRowArr[j]);
-            }
-        }
-        return resultArr;
-    }
-
-    static void Print(int[,] result, int N, int K)
-    {
-        for (var i = 0; i < N; i++)
-        {
-            for (var j = 0; j < K; j++)
-            {
-                Write($"{result[i, j]} ");
-            }
-            WriteLine();
-        }
-    }
+	static void Print(int[,] result, int N, int K)
+	{
+	    for (var i = 0; i < N; i++)
+	    {
+	        for (var j = 0; j < K; j++)
+	        {
+	            Write($"{result[i, j]} ");
+	        }
+	        WriteLine();
+		}
+	}
 }
 
 public static class EnumerableExtensions
 {
-    public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
-    {
-        foreach (T item in enumeration)
-        {
-            action(item);
-        }
-    }
+	public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+	{
+		foreach (T item in enumeration)
+		{
+			action(item);
+		}
+	}
 
-    public static int[] ChangeCharToInt(this IEnumerable<char> enumerable)
-    {
-        return enumerable.Select(item => int.Parse(item.ToString())).ToArray();
-    }
+	public static int[] ChangeCharToInt(this IEnumerable<char> enumerable)
+	{
+		return enumerable.Select(item => int.Parse(item.ToString())).ToArray();
+	}
 
-    public static long[] ChangeCharToLong(this IEnumerable<char> enumerable)
-    {
-        return enumerable.Select(item => long.Parse(item.ToString())).ToArray();
-    }
+	public static long[] ChangeCharToLong(this IEnumerable<char> enumerable)
+	{
+		return enumerable.Select(item => long.Parse(item.ToString())).ToArray();
+	}
 
-    public static int[] ChangeStrToInt(this IEnumerable<string> enumerable)
-    {
-        return enumerable.Select(x => int.Parse(x)).ToArray();
-    }
+	public static int[] ChangeStrToInt(this IEnumerable<string> enumerable)
+	{
+		return enumerable.Select(x => int.Parse(x)).ToArray();
+	}
 
-    public static Queue<T> ToQueue<T>(this IEnumerable<T> enumerable)
-    {
-        var queue = new Queue<T>();
-        enumerable.ForEach(item => queue.Enqueue(item));
-        return queue;
-    }
+	public static Queue<T> ToQueue<T>(this IEnumerable<T> enumerable)
+	{
+		var queue = new Queue<T>();
+		enumerable.ForEach(item => queue.Enqueue(item));
+		return queue;
+	}
 
-    public static Dictionary<int, int> ConvertToDictionary(this IEnumerable<int> enumerable)
-    {
-        return enumerable
-            .GroupBy(height => height)
-            .ToDictionary(group => group.Key, group => group.Count());
-    }
+	public static Dictionary<int, int> ConvertToDictionary(this IEnumerable<int> enumerable)
+	{
+		return enumerable
+			.GroupBy(height => height)
+			.ToDictionary(group => group.Key, group => group.Count());
+	}
 }
 
 public class Result<T>
 {
-    public string Error { get; set; }
-    public T Content { get; set; }
+	public string Error { get; set; }
+	public T Content { get; set; }
 
-    public bool HasContent { get; set; } = false;
+	public bool HasContent { get; set; } = false;
 
-    public static Result<T> Success(T content)
-    {
-        var result = new Result<T>();
-        result.Content = content;
-        result.HasContent = true;
-        return result;
-    }
+	public static Result<T> Success(T content)
+	{
+		var result = new Result<T>();
+		result.Content = content;
+		result.HasContent = true;
+		return result;
+	}
 
-    public static Result<T> Fail(string error = "error")
-    {
-        var result = new Result<T>();
-        result.Error = error;
-        return result;
-    }
+	public static Result<T> Fail(string error = "error")
+	{
+		var result = new Result<T>();
+		result.Error = error;
+		return result;
+	}
 
-    public bool IsSuccess()
-    {
-        return Error == null;
-    }
+	public bool IsSuccess()
+	{
+		return Error == null;
+	}
 
-    public bool IsError()
-    {
-        return Error != null;
-    }
+	public bool IsError()
+	{
+		return Error != null;
+	}
 }
